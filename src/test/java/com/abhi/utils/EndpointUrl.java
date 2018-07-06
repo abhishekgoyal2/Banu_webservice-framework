25a43d0784d57abf03a8adf5f18262c067e22e72
abhishek.goyal2 <abhi25goel@gmail.com>
2018-07-06 16:50:46 +0530

    Commit circuti and endpoint url changes

--- src/test/java/com/abhi/utils/EndpointUrl.java
+++ src/test/java/com/abhi/utils/EndpointUrl.java
@@ -1,5 +1,23 @@
 package com.abhi.utils;
 
-public class EndpointUrl {
+public enum EndpointUrl {
+	
+	Add_circuuit("year/circuits");
 
+	String Resourcepath;
+	
+	EndpointUrl(String Resourcepath)
+	{
+		this.Resourcepath=Resourcepath;
+	}
+	
+	public String getresourcepath()
+	{
+		return this.Resourcepath;
+	}
+	
+	public String getresourcepath(String data)
+	{
+		return this.Resourcepath+data;
+	}
 }
